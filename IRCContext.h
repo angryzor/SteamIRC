6047cfa0efaf49497725f27444d3d7488b1fea15
rtytgat <rtytgat@0f717213-f724-44a5-ba51-6698f9c60e3a>
2009-10-14 13:11:29 +0000

    git-svn-id: svn+ssh://wilma.vub.ac.be/home/rtytgat/MyOwn/SteamIRC/repo@19 0f717213-f724-44a5-ba51-6698f9c60e3a

--- IRCContext.h
+++ IRCContext.h
@@ -7,16 +7,19 @@ namespace SteamIRC {
 	class CIRCContext
 	{
 	public:
+///*
 		enum AcceptReturnValue {
 			ARV_FAIL = 0,
 			ARV_PROCESSED_BUT_CONTINUE = 1,
 			ARV_PROCESSED = 2
 		};
+//*/
 		virtual bool UserInput(const std::string& txt) = 0;
-		virtual AcceptReturnValue AcceptIncoming(const IRCMessage& msg) = 0;
+		virtual AcceptReturnValue AcceptIncoming(const CIRCMessage& msg) = 0;
 		virtual ~CIRCContext(void);
 		std::string GetBuffer() const;
 		std::string GetTitle() const;
+		virtual void Text(const std::string& msg);
 		void SetTitle(std::string newT);
 	protected:
 		CIRCContext(std::string title);
